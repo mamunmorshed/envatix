218ab7a5ded3cebea9f38d0d4a9c433cd15f86cf
Mamun Morshed <mamun@audacityit.com>
2016-11-07 03:36:43 +0600

    After successful testing of the opesnshift, now in this version, guzzle installed via composer & written a simple test script.

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php 
 
-echo "Hello World!";
+require 'vendor/autoload.php';
 
-phpinfo();
+use GuzzleHttp\Client;
+
+$client = new Client([
+	'base-uri' => 'https://httpbin.org/',
+	'timeout' => 2.0,
+]);
+
+$respose = $client->request('GET', 'ip');
+
+var_dump($respose->getStatusCode());
+var_dump($respose->getReasonPhrase());
+
+if ($respose->getHeader('Contetn-Length')) {
+	var_dump($respose->getHeader('Contetn-Length'));
+}
+
+foreach ($response->getHeaders() as $name => $values) {
+    echo $name . ': ' . implode(', ', $values) . "\r\n";
+}
