70f84c16e6441558238999b84cadbba73a52821a
Mamun Morshed <mamun@audacityit.com>
2016-11-07 05:23:06 +0600

    this is the extension of the prior commit

--- index.php
+++ index.php
@@ -3,21 +3,39 @@
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
 
 $client = new Client([
-	'base-uri' => 'https://httpbin.org/',
-	'timeout' => 2.0,
+	'base_uri' => 'https://themeforest.net',
+	// 'timeout' => 2.0,
 ]);
 
-$respose = $client->request('GET', 'ip');
-
-var_dump($respose->getStatusCode());
-var_dump($respose->getReasonPhrase());
-
-if ($respose->getHeader('Contetn-Length')) {
-	var_dump($respose->getHeader('Contetn-Length'));
+try {	
+	$response = $client->request('GET', 'user/themebucket', ['verify' => false]);
+} catch (RequestException $e) {
+	echo psr7\str($e->getRequest());
+	if ($e->hasResponse()) {
+		echo Psr7\str($e->getResponse());
+	}
 }
 
-foreach ($response->getHeaders() as $name => $values) {
-    echo $name . ': ' . implode(', ', $values) . "\r\n";
+if (isset($response)) {
+	var_dump($response->getStatusCode());
+	var_dump($response->getReasonPhrase());
+
+	if ($response->getHeader('Contetn-Length')) {
+		var_dump($response->getHeader('Contetn-Length'));
+	}
+
+	foreach ($response->getHeaders() as $name => $values) {
+	    echo $name . ': ' . implode(', ', $values) . "\r\n";
+	}
+
+	$body = $response->getBody();
+	// Implicitly cast the body to a string and echo it
+	echo $body;
+	echo (string) $body;
+}else{
+	echo "<h1>RETRY</h1>";
 }
